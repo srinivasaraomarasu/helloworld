19b68a574e71f3c08a97b0194ae054e89ebb1cfb
srinivasaraomarasu <srmarasu84@gmail.com>
2016-08-31 00:27:10 +0530

    first program created

---
+++ HelloWorld/src/com/srini/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.srini.hello;
+
+/**
+ * @author sri
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World, this is my first git repository");
+	}
+
+}
